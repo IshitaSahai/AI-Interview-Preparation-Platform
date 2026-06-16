167cec0aebad4f34ac71723891ececad79ff8661
Ishita Sahai <ishitasahai0999@gmail.com>
2026-06-16 09:27:09 +0530

    Restore App component and its dependencies

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,3 @@
-// import { RouterProvider } from "react-router"
-// import {router} from "./app.routes.jsx"
-// import {AuthProvider} from "./features/auth/auth.context.jsx"
-
-// function App() {
-
-//   return (
-//     <AuthProvider>
-//       <RouterProvider router ={router}/>
-//     </AuthProvider>
-//   )
-// }
-
-// export default App
-
-
 import { RouterProvider } from "react-router";
 import { router } from "./app.routes.jsx";
 import { AuthProvider } from "./features/auth/auth.context.jsx";
